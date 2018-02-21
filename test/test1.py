b4b05386c6cf7fce52750b7d3727775db473cd25
Peter Teuben <teuben@gmail.com>
2018-02-21 15:05:50 -0700

    updated model for otf

--- test/test1.py
+++ test/test1.py
@@ -26,12 +26,38 @@ if True:
 ng_vla('test1',model,imsize_m,pixel_m,cfg='../SWcore',ptg=ptg, phasecenter=phasecenter)
 
 # clean this interferometric map a bit
-ng_clean1('test1/clean1','test1/test1.SWcore.ms',  imsize_s, pixel_s, phasecenter=phasecenter,niter=1000)
+ng_clean1('test1/clean1','test1/test1.SWcore.ms',  imsize_s, pixel_s, phasecenter=phasecenter,niter=[0,1000])
 
 # create two OTF maps 
-ng_tp_otf('test1/clean1',model, 45.0, label="45")
-ng_tp_otf('test1/clean1',model, 18.0, label="18")
+ng_tp_otf('test1/clean1','test1/test1.SWcore.skymodel', 45.0, label="45")
+ng_tp_otf('test1/clean1','test1/test1.SWcore.skymodel', 18.0, label="18")
 
 # combine TP + INT using feather
 ng_feather('test1/clean1',label="45")
 ng_feather('test1/clean1',label="18")
+
+#
+print "Done!"
+
+# --------------------------------------------------------------------------------------------------------------
+# regression
+
+regress51 = [
+    "0.0067413167369069988 0.010552344105427177 0.0 0.10000000149011612 113100.52701950389",
+    "411.08972165273946 821.42796910126435 0.070715504411804908 21357.570702738558 0.0",
+    ]
+
+
+r = regress51
+    
+
+# regression
+ng_stats(model,                                 r[0])
+ng_stats('test1/test1.SWcore.ms',               r[1])
+ng_stats('test1/clean1/dirtymap.image')
+ng_stats('test1/clean1/otf45.image')
+ng_stats('test1/clean1/otf18.image.pbcor')
+ng_stats('test1/clean1/otf45.image')
+ng_stats('test1/clean1/otf18.image.pbcor')
+ng_stats('test1/clean1/feather.image')
+ng_stats('test1/clean1/feather.image.pbcor')
