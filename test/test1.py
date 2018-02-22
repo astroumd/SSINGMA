6e39de1a30156745e9e584807f31157caf142a58
Peter Teuben <teuben@gmail.com>
2018-02-22 09:15:54 -0700

    docs, use ng_ptg now.

--- test/test1.py
+++ test/test1.py
@@ -7,30 +7,28 @@
 # 2073.348u 37.540s 30:59.81 113.4%	0+0k 2335376+3269568io 887pf+0w     niter=[0,1000,2000]
 
 
-model        = '../models/skymodel.fits'           # this as phasecenter with dec=-30 for ALMA sims
-phasecenter  = 'J2000 180.000000deg 40.000000deg'
-ptg          = 'test1.ptg'
+model        = '../models/skymodel.fits'           # this has phasecenter with dec=-30 for ALMA sims
+phasecenter  = 'J2000 180.000000deg 40.000000deg'  # so modify this for ngVLA
+ptg          = 'test1.ptg'                         # and use a single pointing mosaic for the ptg
 
 # pick the piece of the model to image, and at what pixel size
 imsize_m     = 4096
 pixel_m      = 0.01
 
-# pick the imaging parameters 
+# pick the sky imaging parameters (for tclean)
 imsize_s     = 512
 pixel_s      = 0.25
 
+niter        = [0,1000,2000]
 
-if True:
-    # need a better way?  an ng_()
-    fp = open(ptg,"w")
-    fp.write("%s" % phasecenter)
-    fp.close()
+# create a single pointing mosaic
+ng_ptg(phasecenter,ptg)
 
 # create a MS based on a model and antenna configuration
 ng_vla('test1',model,imsize_m,pixel_m,cfg='../SWcore',ptg=ptg, phasecenter=phasecenter)
 
 # clean this interferometric map a bit
-ng_clean1('test1/clean1','test1/test1.SWcore.ms',  imsize_s, pixel_s, phasecenter=phasecenter,niter=[0,1000,2000])
+ng_clean1('test1/clean1','test1/test1.SWcore.ms',  imsize_s, pixel_s, phasecenter=phasecenter,niter=niter)
 
 # create two OTF maps 
 ng_tp_otf('test1/clean1','test1/test1.SWcore.skymodel', 45.0, label="45")
