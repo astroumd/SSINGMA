338617c035cc6fa6fb09dbb367ad79bae16532a3
Peter Teuben <teuben@gmail.com>
2018-02-23 14:49:10 -0500

    example command line parsing ; use generic base name as variable

--- test/test1.py
+++ test/test1.py
@@ -6,10 +6,9 @@
 # 667.692u 20.628s 9:45.15 117.6%	0+0k 1121096+3180192io 335pf+0w     niter=0
 # 2073.348u 37.540s 30:59.81 113.4%	0+0k 2335376+3269568io 887pf+0w     niter=[0,1000,2000]
 
-
+test         = 'test1'
 model        = '../models/skymodel.fits'           # this has phasecenter with dec=-30 for ALMA sims
 phasecenter  = 'J2000 180.000000deg 40.000000deg'  # so modify this for ngVLA
-ptg          = 'test1.ptg'                         # and use a single pointing mosaic for the ptg
 
 # pick the piece of the model to image, and at what pixel size
 # natively this model is 4096 pixels at 0.05"
@@ -23,7 +22,13 @@ pixel_s      = 0.25
 # pick a few niter values for tclean to check flux convergence 
 niter        = [0,1000,2000]
 
+# -- do not change parameters below this ---
+import sys
+for arg in ng_argv(sys.argv):
+    exec(arg)
 
+ptg = test + '.ptg'              # use a single pointing mosaic for the ptg
+    
 
 # report
 ng_version()
@@ -32,19 +37,19 @@ ng_version()
 ng_ptg(phasecenter,ptg)
 
 # create a MS based on a model and antenna configuration
-ng_vla('test1',model,imsize_m,pixel_m,cfg='../SWcore',ptg=ptg, phasecenter=phasecenter)
+ng_vla(test,model,imsize_m,pixel_m,cfg='../SWcore',ptg=ptg, phasecenter=phasecenter)
 
 # clean this interferometric map a bit
-ng_clean1('test1/clean1','test1/test1.SWcore.ms',  imsize_s, pixel_s, phasecenter=phasecenter,niter=niter)
+ng_clean1(test+'/clean1',test+'/'+test+'.SWcore.ms',  imsize_s, pixel_s, phasecenter=phasecenter,niter=niter)
 
 # create two OTF TP maps 
-ng_tp_otf('test1/clean1','test1/test1.SWcore.skymodel', 45.0, label="45")
-ng_tp_otf('test1/clean1','test1/test1.SWcore.skymodel', 18.0, label="18")
+ng_tp_otf(test+'/clean1',test+'/'+test+'.SWcore.skymodel', 45.0, label="45")
+ng_tp_otf(test+'/clean1',test+'/'+test+'.SWcore.skymodel', 18.0, label="18")
 
 # combine TP + INT using feather, for all niter's
 for idx in range(len(niter)):
-    ng_feather('test1/clean1',label="45",niteridx=idx)
-    ng_feather('test1/clean1',label="18",niteridx=idx)
+    ng_feather(test+'/clean1',label="45",niteridx=idx)
+    ng_feather(test+'/clean1',label="18",niteridx=idx)
 
 #
 print "Done!"
