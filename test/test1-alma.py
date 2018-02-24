62a140d9583398313e3005c3da4030c965e25dcf
Peter Teuben <teuben@gmail.com>
2018-02-24 04:04:55 -0500

    optimized size of imaging area

--- test/test1-alma.py
+++ test/test1-alma.py
@@ -1,7 +1,7 @@
 #
 #
 #  like test1, but for alma, just for kicks
-#  The 12m PB is 50" (FWHM)
+#  The 12m PB is 50" (FWHM)   [FWHM" ~ 600/DishDiam]
 #       7m PB is 85"
 #
 
@@ -16,10 +16,12 @@ pixel_m      = 0.01
 
 # pick the sky imaging parameters (for tclean)
 imsize_s     = 256
-pixel_s      = 0.5
+pixel_s      = 0.16
 
 # pick a few niter values for tclean to check flux convergence 
 niter        = [0,500]
+
+cfg          = [0,1,2,3,4,5]
 
 # -- do not change parameters below this ---
 import sys
@@ -30,29 +32,38 @@ ptg = test + '.ptg'              # use a single pointing mosaic for the ptg
     
 
 # report
+ng_log("REPORT")
 ng_version()
 
 # create a single pointing mosaic
 ng_ptg(phasecenter,ptg)
 
-# create a MS based on a model and antenna configuration
-ng_alma(test,model,imsize_m,pixel_m,cycle=5,cfg=1,ptg=ptg, phasecenter=phasecenter)
+# create an MS based on a model and antenna configuration for
+ng_log("NG_ALMA")
+for c in cfg:
+    ms1 = ng_alma(test,model,imsize_m,pixel_m,cycle=5,cfg=c,ptg=ptg, phasecenter=phasecenter)
+
+# startmodel (a cheat)
+startmodel = ms1.replace('.ms','.skymodel')
 
 # find out which MS we got:
 import glob
 mslist = glob.glob(test+"/*.ms")
 print "MS files found: ",mslist
 
+ng_log("CLEAN")
 # clean this interferometric map a bit
 ng_clean1(test+'/clean1',mslist,  imsize_s, pixel_s, phasecenter=phasecenter,niter=niter)
-ng_clean1(test+'/clean2',mslist,  imsize_s, pixel_s, phasecenter=phasecenter,niter=niter,startmodel=model)
+ng_clean1(test+'/clean2',mslist,  imsize_s, pixel_s, phasecenter=phasecenter,niter=niter,startmodel=startmodel)
 
-# create two OTF TP maps 
-ng_tp_otf(test+'/clean1',test+'/'+test+'.SWcore.skymodel', 12.0)
+ng_log("OTF")
+# create an OTF TP map
+ng_tp_otf(test+'/clean1', startmodel, 12.0)
 
+ng_log("FEATHER")
 # combine TP + INT using feather, for all niter's
 for idx in range(len(niter)):
     ng_feather(test+'/clean1',niteridx=idx)
-
+    ng_smooth(test+'/clean1', startmodel, niteridx=idx)
 #
-print "Done!"
+ng_log("DONE!")
